6ba9a9b96be6f9f946a5c453f65bca6eefd0d636
Meylishh <murmeylishh@gmail.com>
2024-02-06 17:11:49 +0300

    gameManager ext

--- Assets/Script/Managers/GameManager.cs
+++ Assets/Script/Managers/GameManager.cs
@@ -8,10 +8,25 @@ public class GameManager : MonoBehaviour
     [Header("Clicker")] 
     public static int Clicks;
     public static int Multiplier;
+
+    [Header("Interface")] 
+    [SerializeField] private GameObject clicker;
+    [SerializeField] private GameObject clickerBuffsShC
+    [SerializeField] private GameObject skypeNotification;
     
     private void Start()
     {
+        ///////////////
         Clicks = 0;
         Multiplier = 1;
+        //////////////
+        
+        clicker.SetActive(false);
+        clickerBuffsShop.SetActive(false);
+    }
+
+    public void GameBegin()
+    {
+        
     }
 }
