53dbb6bf3661f64ecb8497ab9f6cc5c68831453a
Meylishh <murmeylishh@gmail.com>
2024-04-01 03:49:16 +0300

    mixer app

---
+++ Assets/Script/Apps/SmallStuff/Mixer/MixerApp.cs
@@ -0,0 +1,37 @@
+using System;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using Script.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MixerApp : MonoBehaviour, IWindowedApp
+{
+    [SerializeField] private Button closeButton;
+    [SerializeField] private float scaleOnOpen;
+    private Vector2 initialPosition;
+    private void Start()
+    {
+        gameObject.SetActive(false);
+        initialPosition = gameObject.transform.position;
+        closeButton.onClick.AddListener(CloseApp);
+    }
+
+    public void OpenApp()
+    {
+        OpenAppAsync().Forget();
+    }
+    private async UniTask OpenAppAsync()
+    {
+        gameObject.SetActive(true);
+        closeButton.interactable = false;
+        await gameObject.transform.DOScale(scaleOnOpen, 0.1f).SetLoops(2, LoopType.Yoyo);
+        closeButton.interactable = true;
+    }
+
+    public void CloseApp()
+    {
+        gameObject.SetActive(false);
+        gameObject.transform.position = initialPosition;
+    }
+}
