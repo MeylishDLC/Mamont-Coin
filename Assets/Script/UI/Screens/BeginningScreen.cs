c84132924a09d4351ca97bd130873633fda732de
Meylishh <meylishy20@gmail.com>
2024-05-20 11:06:07 +0300

    FMOD fix for webGl

--- Assets/Script/UI/Screens/BeginningScreen.cs
+++ Assets/Script/UI/Screens/BeginningScreen.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,10 +7,36 @@ namespace Script.UI.Screens
 {
     public class BeginningScreen : MonoBehaviour
     {
+        [FMODUnity.BankRef]
+        public List<string> banks;
         public void OnPasswordEnter()
         {
             //todo: maybe some funny check for password
-            SceneManager.LoadScene(1);
+            LoadBanks();
+            SceneManager.LoadScene(1, LoadSceneMode.Single);
+        }
+        private void LoadBanks()
+        {
+            foreach (string b in banks)
+            {
+                FMODUnity.RuntimeManager.LoadBank(b, true);
+                Debug.Log("Loaded bank " + b);
+            }
+            /*
+                For Chrome / Safari browsers / WebGL.  Reset audio on response to user interaction (LoadBanks is called from a button press), to allow audio to be heard.
+            */
+            FMODUnity.RuntimeManager.CoreSystem.mixerSuspend();
+            FMODUnity.RuntimeManager.CoreSystem.mixerResume();
+            
+            StartCoroutine(CheckBanksLoaded());
+        }
+
+        IEnumerator CheckBanksLoaded()
+        {
+            while (!FMODUnity.RuntimeManager.HaveAllBanksLoaded)
+            {
+                yield return null;
+            }
         }
     }
 }
